87c467d7053d418a0b92567d9679d5bbb48db712
Mhaemnn <ismuhaimin1@gmail.com>
2022-03-14 22:43:44 +0700

    modifikasi

---
+++ #31 DOM Selection/script.js
@@ -0,0 +1,26 @@
+//DOM Selection
+//documnet.getElementById('')  -->mengembalikan element
+
+const judul = document.getElementById('judul');
+judul.style.color = "black";
+judul.style.backgroundColor = "#aaa";
+judul.style.textAlign = "center";
+judul.innerHTML = "Mhaemnn"
+judul.style.textShadow = "1px 1px 2px white";
+
+
+//document.GetElementsByTagName('') --> mengembalikan HTML Colection
+const paragraf = document.getElementsByTagName('p');
+for (let i = 0; i < paragraf.length; i++) {
+    paragraf[i].style.backgroundColor = "#eaeaea";
+}
+
+for (let i = 0; i < paragraf.length; i++) {
+    paragraf[i].style.color = "black";
+    paragraf[i].style.textShadow = "1px 1px 2px white"
+}
+
+
+//document.getElementByClassName('') -->mengembalikan HTML Colection
+const paragraf1 = document.getElementsByClassName('p1');
+paragraf1[0].innerHTML = "DOM Javascript"
---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+## Javascipt & Javascript DOM
+
+__javascript__: <br>
+__javascript DOM__: Adalah antarmuka pemrograman untuk HTML yang mempresentasikan halaman web, sehingga program dapat mengubah dan memanipulasi strukturnya __-Mozilla__
+
+<ul>
+    <li>Antarmuka pemrograman berbasis object yang mempresentasikan dokumet web</li>
+    <li>DOM membuat seluruh komponen dari halaman web dapat diakses & dimanipulasi</li>
+    <li>komponen</li>
+        <ul>
+        <li>Element HTML</li>
+        <li>Atribut</li>
+        <li>Text</li>
+        <li>dll</li>
+        </ul>
+</ul>
