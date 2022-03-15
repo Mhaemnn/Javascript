4a5cfcbc96a5d2eadfedf414b2d4bca446c745ac
Mhaemnn <ismuhaimin1@gmail.com>
2022-03-15 18:43:16 +0700

    modifikasi" "

---
+++ #32 DOM Manupulasi/javascript.js
@@ -0,0 +1,26 @@
+//Manipulasi Element
+//1. element.innerHTML
+//2. element.style.<property>
+//3. element.setAttribut()
+//4. element.classListt
+
+
+//element.innerHTML
+const judul = document.getElementById('judul');
+judul.innerHTML = 'Mhaemnn'
+
+const button = document.getElementById('button');
+button.style.textDecoration = "none"
+
+//element.style.<property>
+const title = document.querySelector('#judul');
+title.style.color = "white";
+title.style.backgroundColor = "salmon"
+
+
+// const a = document.querySelector('section#a a');
+// a 
+
+
+const p2 = document.querySelector('.p2');
+p2
