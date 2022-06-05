87ee8bffa6c73337d0125ac62bdd5ba38802bc29
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-05 22:04:19 +0700

    modifiksi

--- #17 Function: parameter & argument/script.js
+++ #17 Function: parameter & argument/script.js
@@ -127,7 +127,41 @@ console.log(myAge("2022-06-05"));
 
 
 // TODO
+// TODO
+class Animal {
+    constructor(name, age, isMammal) {
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    };
+};
+class Rabbit extends Animal {
+    constructor(name, age) {
+        super(name, age, true)
+    }
+    eat() {
+        return `${this.name} sedang makan!`;
+    }
+}
+class Eagle extends Animal {
+    constructor(name, age) {
+        super(name, age, false)
+    }
+    fly() {
+        return `${this.name} sedang terbang!`;
+    }
+}
+const myRabbit = new Rabbit("Labi", 2);
+const myEagle = new Eagle("Elo", 4);
 
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+    Animal, Rabbit, Eagle, myRabbit, myEagle,
+};
 
 
 /**
