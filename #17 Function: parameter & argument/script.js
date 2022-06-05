c45dd51388bca1695bb5ece65bfcccf05caadcc2
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-05 10:00:11 +0700

    modifikasi

--- #17 Function: parameter & argument/script.js
+++ #17 Function: parameter & argument/script.js
@@ -4,3 +4,32 @@ var tambah = function (a, b) { //? a,b adalah peremeter
 }
 console.log("hasil dari per tambahan = " + tambah(50, 50)); //TODO 50,50 adalah argument
 
+const youtClassName = (paramA, paramB) => {
+    const self = {
+        paramA,
+        paramB
+    };
+
+
+    const yourSpecificAction = self => ({
+        specificAction: {/* */ },
+    });
+
+}
+
+
+// membuat kumpulan antribute, generic method, dan spesific method menjadi satu objek
+
+const yourClassName = (paramA, paramB) => {
+    const self = {
+        paramA,
+        paramB
+    }
+
+    const yourSpecificAction = self => ({
+        specificAction: {/*do action A */ }
+    });
+
+    return Object.assign(self, yourGenericAction(self), yourSpecificAction(self),)
+}
+
