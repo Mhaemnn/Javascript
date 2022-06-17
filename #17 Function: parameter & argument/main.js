2673b1bf707029499f62f007fbdbd8ecbf2c683c
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-17 11:11:26 +0700

    modifikasi

--- #17 Function: parameter & argument/main.js
+++ #17 Function: parameter & argument/main.js
@@ -15,3 +15,29 @@ const grindCoffeeBeans = () => {
         }, 1000);
     })
 }
+function makeEspresso() {
+    checkAvailability()
+        .then((value) => {
+            console.log(value);
+            return checkStock();
+        })
+        .then(value => {
+            console.log(value);
+            const promises = [boilWater(), grindCoffeeBeans()];
+            return Promise.all(promises);
+        })
+        .then((value) => {
+            console.log(value)
+            return brewCoffee();
+        })
+        .then(value => {
+            console.log(value);
+            state.isCoffeeMachineBusy = false;
+        })
+        .catch(rejectedReason => {
+            console.log(rejectedReason);
+            state.isCoffeeMachineBusy = false;
+        });
+}
+
+makeEspresso();
