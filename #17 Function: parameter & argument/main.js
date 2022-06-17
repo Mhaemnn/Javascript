6b8d34bbbacd9cdaf67cc20e1058269cc9cddeaa
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-18 06:02:11 +0700

    modifikasi

--- #17 Function: parameter & argument/main.js
+++ #17 Function: parameter & argument/main.js
@@ -3,3 +3,26 @@ function makeCoffee() {
     console.log(coffee);
 }
 makeCoffee();
+
+
+//callback
+function makeCake() {
+    getCake(function (cake) {
+        console.log(cake)
+    })
+}
+
+makeCake();
+
+// I wear a shirt and a skirt = 
+// she wears a skirt = dia mengenakan sebuah rok
+/*
+hat = topi
+pants = celana
+they wear pants and shirts = mereka mengenakan celana dan kemeja
+clothes = pakaian
+suit = setelan
+they are not yout pants = ini bukan celanamu
+our hat = topi kami
+    
+ */
