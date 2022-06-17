f9c97827d56e010528b6db26e95432c4c1e46b7d
Mhaemnn <ismuhaimin1@gmail.com>
2022-06-17 11:15:31 +0700

    modifikasi

--- #17 Function: parameter & argument/main.js
+++ #17 Function: parameter & argument/main.js
@@ -1,43 +1,5 @@
-const boilWater = () => {
-    return new Promise((resolve, reject) => {
-        console.log("Memanaskan air...");
-        setTimeout(() => {
-            resolve("Air panas sudah siap!");
-        }, 2000);
-    })
+function makeCoffee() {
+    const coffee = getCoffee(); //asyc process menggunakan promise
+    console.log(coffee);
 }
-
-const grindCoffeeBeans = () => {
-    return new Promise((resolve, reject) => {
-        console.log("Menggiling biji kopi...");
-        setTimeout(() => {
-            resolve("Kopi sudah siap!");
-        }, 1000);
-    })
-}
-function makeEspresso() {
-    checkAvailability()
-        .then((value) => {
-            console.log(value);
-            return checkStock();
-        })
-        .then(value => {
-            console.log(value);
-            const promises = [boilWater(), grindCoffeeBeans()];
-            return Promise.all(promises);
-        })
-        .then((value) => {
-            console.log(value)
-            return brewCoffee();
-        })
-        .then(value => {
-            console.log(value);
-            state.isCoffeeMachineBusy = false;
-        })
-        .catch(rejectedReason => {
-            console.log(rejectedReason);
-            state.isCoffeeMachineBusy = false;
-        });
-}
-
-makeEspresso();
+makeCoffee();
